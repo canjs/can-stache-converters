8437ac81393880725370570cdc6c821cac1c380d
Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2019-05-10 18:55:29 +0100

    Landscaper: QUnit2 upgrade

--- test/equal_test.js
+++ test/equal_test.js
@@ -9,72 +9,72 @@ var QUnit = require("steal-qunit");
 
 QUnit.module("can-stache-converters: equal");
 
-QUnit.test("Basics works", function(){
+QUnit.test("Basics works", function(assert) {
 	var template = stache('<input type="radio" checked:bind="equal(~attending, \'yes\'" /><input type="radio" checked:bind="equal(~attending, \'no\'" />');
 	var attending = compute("yes");
 
 	var yes = template({ attending: attending }).firstChild,
 		no = yes.nextSibling;
 
-	QUnit.equal(yes.checked, true, "initially a yes");
-	QUnit.equal(no.checked, false, "initially unchecked");
+	assert.equal(yes.checked, true, "initially a yes");
+	assert.equal(no.checked, false, "initially unchecked");
 
 	attending("no");
 
-	QUnit.equal(yes.checked, false, "now not checked");
-	QUnit.equal(no.checked, true, "now checked");
+	assert.equal(yes.checked, false, "now not checked");
+	assert.equal(no.checked, true, "now checked");
 
 	// User changing stuff
 	yes.checked = true;
 	domEvents.dispatch(yes, "change");
 
-	QUnit.equal(attending(), "yes", "now it is yes");
-	QUnit.equal(yes.checked, true, "yes is checked");
-	QUnit.equal(no.checked, false, "no is unchecked");
+	assert.equal(attending(), "yes", "now it is yes");
+	assert.equal(yes.checked, true, "yes is checked");
+	assert.equal(no.checked, false, "no is unchecked");
 });
 
-QUnit.test("works without ~", function(){
+QUnit.test("works without ~", function(assert) {
 	var template = stache('<input type="radio" checked:bind="equal(attending, \'yes\'" /><input type="radio" checked:bind="equal(~attending, \'no\'" />');
 	var attending = compute("yes");
 
 	var yes = template({ attending: attending }).firstChild,
 		no = yes.nextSibling;
 
-	QUnit.equal(yes.checked, true, "initially a yes");
-	QUnit.equal(no.checked, false, "initially unchecked");
+	assert.equal(yes.checked, true, "initially a yes");
+	assert.equal(no.checked, false, "initially unchecked");
 
 	attending("no");
 
-	QUnit.equal(yes.checked, false, "now not checked");
-	QUnit.equal(no.checked, true, "now checked");
+	assert.equal(yes.checked, false, "now not checked");
+	assert.equal(no.checked, true, "now checked");
 
 	// User changing stuff
 	yes.checked = true;
 	domEvents.dispatch(yes, "change");
 
-	QUnit.equal(attending(), "yes", "now it is yes");
-	QUnit.equal(yes.checked, true, "yes is checked");
-	QUnit.equal(no.checked, false, "no is unchecked");
+	assert.equal(attending(), "yes", "now it is yes");
+	assert.equal(yes.checked, true, "yes is checked");
+	assert.equal(no.checked, false, "no is unchecked");
 });
 
-QUnit.test("Allows one-way binding when passed a non-compute as the first argument", function(){
+QUnit.test("Allows one-way binding when passed a non-compute as the first argument", function(assert) {
 	var template = stache('<input type="radio" checked:bind="equal(attending, true)" />');
 	var attending = compute(false);
 
 	var input = template({ attending: attending }).firstChild;
 
-	QUnit.equal(input.checked, false, 'initially false');
+	assert.equal(input.checked, false, 'initially false');
 
 	attending(true);
 
-	QUnit.equal(input.checked, true, 'can be changed to true');
+	assert.equal(input.checked, true, 'can be changed to true');
 
 	input.checked = false;
 
-	QUnit.equal(attending(), true, 'does not change compute');
+	assert.equal(attending(), true, 'does not change compute');
 });
 
-QUnit.test("Allow multiple expressions to be passed in", function() {
+QUnit.test("Allow multiple expressions to be passed in", function(assert) {
 	var template = stache('<input type="radio" checked:bind="equal(~foo, ~bar, true)" />');
 	var foo = compute(true);
 	var bar = compute(false);
@@ -84,25 +84,25 @@ QUnit.test("Allow multiple expressions to be passed in", function() {
 		bar: bar
 	}).firstChild;
 
-	QUnit.equal(input.checked, false, 'initially unchecked');
+	assert.equal(input.checked, false, 'initially unchecked');
 
 	bar(true);
 
-	QUnit.equal(input.checked, true, 'now checked');
+	assert.equal(input.checked, true, 'now checked');
 
 	foo(false);
 	bar(false);
 
-	QUnit.equal(input.checked, false, 'now unchecked');
+	assert.equal(input.checked, false, 'now unchecked');
 
 	input.checked = true;
 	domEvents.dispatch(input, "change");
 
-	QUnit.equal(foo(), true, 'computed foo value is true');
-	QUnit.equal(bar(), true, 'computed bar value is true');
+	assert.equal(foo(), true, 'computed foo value is true');
+	assert.equal(bar(), true, 'computed bar value is true');
 });
 
-QUnit.test("Allow multiple expressions to be passed in without ~", function() {
+QUnit.test("Allow multiple expressions to be passed in without ~", function(assert) {
 	var template = stache('<input type="radio" checked:bind="equal(foo, bar, true)" />');
 	var foo = compute(true);
 	var bar = compute(false);
@@ -112,25 +112,25 @@ QUnit.test("Allow multiple expressions to be passed in without ~", function() {
 		bar: bar
 	}).firstChild;
 
-	QUnit.equal(input.checked, false, 'initially unchecked');
+	assert.equal(input.checked, false, 'initially unchecked');
 
 	bar(true);
 
-	QUnit.equal(input.checked, true, 'now checked');
+	assert.equal(input.checked, true, 'now checked');
 
 	foo(false);
 	bar(false);
 
-	QUnit.equal(input.checked, false, 'now unchecked');
+	assert.equal(input.checked, false, 'now unchecked');
 
 	input.checked = true;
 	domEvents.dispatch(input, "change");
 
-	QUnit.equal(foo(), true, 'computed foo value is true');
-	QUnit.equal(bar(), true, 'computed bar value is true');
+	assert.equal(foo(), true, 'computed foo value is true');
+	assert.equal(bar(), true, 'computed bar value is true');
 });
 
-QUnit.test("Allow non static values", function() {
+QUnit.test("Allow non static values", function(assert) {
 	var template = stache('{{# each(foo) }}<input type="radio" checked:bind="equal(., ../bar.value)" />{{/ each}}');
 	var foo = new DefineList([ 'foobar' ]);
 	var bar = new DefineMap({value: 'zed'});
@@ -140,9 +140,9 @@ QUnit.test("Allow non static values", function() {
 		bar: bar
 	}).querySelector('input');
 
-	QUnit.equal(input.checked, false, 'initially unchecked');
+	assert.equal(input.checked, false, 'initially unchecked');
 
 	bar.value = 'foobar';
 
-	QUnit.equal(input.checked, true, 'now checked');
+	assert.equal(input.checked, true, 'now checked');
 });
